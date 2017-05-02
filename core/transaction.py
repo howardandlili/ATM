4d62b627e21f98b9f3fd02e2183389ef7ebe55c4
Administrator <1155090@qq.com>
2017-05-03 02:10:32 +0800

    加入交易模块但是还没有写混合运算

---
+++ core/transaction.py
@@ -0,0 +1,24 @@
+#!/user/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'Howie'
+import os,sys
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base_dir)
+from conf import setting
+'''
+这是一个交易的模块
+主要是计算还款、取款、转款的交易
+'''
+def make_transaction(**kwargs):
+    amount = kwargs['amount']
+    tran_type = kwargs['tran_type']
+    old_balance = kwargs['acc_data']['balance']
+    interest = setting.tran_type[tran_type]['interest']
+    iamount = amount*(1+interest)
+    interest = amount*interest
+    print('变动金额为：',amount,'利息为：',interest)
+    #由于交易类型都加或者减，不过还是需要计算利息那么这个就是conf那里配置好就可以了
+    if tran_type in setting.tran_type:
+        print('开始运算')
+        new_balance = old_balance + iamount
+        return new_balance

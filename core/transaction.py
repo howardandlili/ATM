fed289e2450caf67f30fdc7d51b49b404d260c43
Administrator <1155090@qq.com>
2017-05-03 16:38:30 +0800

    优化了参数的获取

--- core/transaction.py
+++ core/transaction.py
@@ -11,17 +11,17 @@ from conf import setting
 '''
 def make_transaction(**kwargs):
     amount = kwargs['amount']
-    tran_type = kwargs['tran_type']
-    old_balance = kwargs['acc_data']['balance']
+    tran_type = kwargs.get('tran_type')
+    old_balance = (kwargs.get('acc_data')).get('balance')
     interest = setting.tran_type[tran_type]['interest']
-    iamount = amount*(1+interest)
+    amount = amount*(1+interest)
     interest = amount*interest
     print('变动金额为：',amount,'利息为：',interest)
     #由于交易类型都加或者减，不过还是需要计算利息那么这个就是conf那里配置好就可以了
     if tran_type in setting.tran_type:
         action = setting.tran_type[tran_type]['action']
         if action == 'plus':
-            new_balance = old_balance + iamount
+            new_balance = old_balance + amount
         else:
-            new_balance = old_balance - iamount
+            new_balance = old_balance - amount
         return new_balance
